0e1a7a986fd2815080075a54df8b3e1585f4831b
MiikeT <mikemtaki@gmail.com>
2020-05-29 12:09:14 -0700

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,5 @@
 def line(katz_deli)
-  if katz_deli.size == 0
+  if katz_deli.empty?
     puts "The line is currently empty."
   else
     string = "The line is currently:"
@@ -13,12 +13,12 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 
 def now_serving(katz_deli)
-  if katz_deli.size == 0
+  if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{katz_deli[0]}."
